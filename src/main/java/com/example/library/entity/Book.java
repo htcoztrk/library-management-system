b25f12181b304a7b8eb4069da5ecdd98d8b2c9ff
mazlumgurbuz <mazlumgurbuz30@gmail.com>
2022-03-25 12:16:41 +0300

    update book

--- src/main/java/com/example/library/entity/Book.java
+++ src/main/java/com/example/library/entity/Book.java
@@ -1,17 +1,21 @@
 package com.example.library.entity;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.DynamicUpdate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -26,13 +30,18 @@ public class Book {
 	private Long id;
 	@NotBlank(message="isbn can not be blank")
 	private String  isbn;
+	@ManyToOne()
 	private Author author;
 	@NotBlank(message="title can not be blank")
 	@NotNull
 	private String title;
 	private byte[] qrCode;
-	private Date pressDate;
+	private Category category;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private LocalDate pressDate;
+	private Publisher publisher;
 	@Enumerated
 	private BookType bookType;
-	//private Publisher publisher;
+	private boolean isBorrowed;
+	private boolean isReserved;
 }
